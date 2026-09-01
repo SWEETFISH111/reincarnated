a2810256f2568422bd6e10ea7eba2e4d8af579ca
seel110 <ta23020610@gmail.com>
2026-09-01 20:17:57 +0900

    feat:SkillScreenに画像表示

--- src/main/java/com/github/sweetfish111/reincarnated/client/screen/skill/SkillEditorScreen.java
+++ src/main/java/com/github/sweetfish111/reincarnated/client/screen/skill/SkillEditorScreen.java
@@ -1,12 +1,18 @@
 package com.github.sweetfish111.reincarnated.client.screen.skill;
 
 import com.github.sweetfish111.reincarnated.circuit.EditorTab;
+import com.github.sweetfish111.reincarnated.client.screen.WorkspaceCamera;
 import com.github.sweetfish111.reincarnated.client.screen.magic.AbstractEditorScreen;
+import com.github.sweetfish111.reincarnated.reincarnated;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.input.MouseButtonEvent;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.Identifier;
 
 public class SkillEditorScreen extends AbstractEditorScreen {
+    private Identifier TEXTURE = Identifier.fromNamespaceAndPath(reincarnated.MODID, "textures/gui/container/スキルスロット1x1");
+    private WorkspaceCamera camera = new WorkspaceCamera();
+
     public SkillEditorScreen(Component title) {
         super(title);
     }
@@ -20,7 +26,13 @@ public class SkillEditorScreen extends AbstractEditorScreen {
 
     @Override
     public void extractRenderState(GuiGraphicsExtractor graphics, int mouseX, int mouseY, float a) {
+        graphics.pose().pushMatrix();
+        graphics.pose().translate((float)this.camera.panX, (float)this.camera.panY);
+        graphics.pose().scale(this.camera.zoom, this.camera.zoom);
 
+        graphics.blit(TEXTURE, 200, 100, 0, 0, 176, 166, 176, 166);
+
+        graphics.pose().popMatrix();
         super.extractRenderState(graphics, mouseX, mouseY, a);
     }
 
